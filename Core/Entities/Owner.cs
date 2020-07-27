375d472898aab80005907e6e8b53a6e338ec7c60
Osama Bisheer <osamamohammed9694@gmail.com>
2020-07-27 03:00:18 +0200

    Add project files.

---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities
+{
+    public class Owner :EntityBase
+    {
+        public string FullName { get; set; }
+        public string Postion { get; set; }
+        public string Avatar { get; set; }
+    }
+}
